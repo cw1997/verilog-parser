de4db53f3a8d56231b549fe046a47cfb2a9d1d83
昌维 <867597730@qq.com>
2021-06-11 03:20:36 +0800

    update sample verilog file: add always block and port item

--- add2.v
+++ add2.v
@@ -2,13 +2,13 @@ module add2 #(
     clock_freq_hz = 50_000_000,
     clock_ns_per_hz = 20
 ) (
-    input  add1,
-    input  add2,add3,
-    output result_add,
-    output result_sub,
-    output result_ori,
-    input  clk, 
-    input  rst_n
+    input  wire [ 7:0] add1,
+    input  wire [ 7:0] add2,add3, add4,
+    output reg  [ 8:0] result_add,
+    output reg  [ 8:0] result_sub,
+    output      [ 8:0] result_ori,      result_and,
+    output      [ 8:0] result,
+    input              clk, rst_n
 );
 
 // this is comment
@@ -18,10 +18,27 @@ assign result_ori = 9'b0_1234_5678;
 always @(posedge clk or negedge rst_n) begin
     result <= add1 + add2 + add3;
     if (~rst_n) begin
-        result <= 9'b0;
+        result_add <= 0;
+        result_sub <= 0;
     end else begin
         result_add <= add1 + add2;
         result_sub <= add1 - add2;
+    end
+    
+    // result 2
+    if (~rst_n) begin
+        result <= 9'b0;
+    end else begin
+        result <= 9'b0_1234_5678;
+    end
+end
+
+// result_and
+always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        result_and <= 9'b0;
+    end else begin
+        result_and <= add1 & add2;
     end
 end
     
