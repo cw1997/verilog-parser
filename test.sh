4237efb854e1d7023011d080f38d30de2b26b44b
昌维 <867597730@qq.com>
2021-06-11 21:51:05 +0800

    fix an error about antlr TestRig input java class name: add package name

--- test.sh
+++ test.sh
@@ -12,10 +12,9 @@ OUTPUT_DIRECTORY='./verilog'
 SUB_DIRECTORY='/me/changwei/verilog/parser'
 OUTPUT_SRC_DIRECTORY="${OUTPUT_DIRECTORY}/target/generated-sources${SUB_DIRECTORY}"
 OUTPUT_BIN_DIRECTORY="${OUTPUT_DIRECTORY}/target/classes"
-EXECUTABLE_BIN_DIRECTORY="${OUTPUT_BIN_DIRECTORY}"
 
 java org.antlr.v4.Tool -listener -visitor -long-messages ./src/${NAME}.g4 -o ${OUTPUT_SRC_DIRECTORY} -package ${PACKAGE} && \
 mkdir -p ${OUTPUT_BIN_DIRECTORY} && \
 javac ${OUTPUT_SRC_DIRECTORY}/*.java -d ${OUTPUT_BIN_DIRECTORY} && \
-cd ${EXECUTABLE_BIN_DIRECTORY} && \
-java org.antlr.v4.gui.TestRig ${NAME} ${START} -gui ${FILE}
+cd ${OUTPUT_BIN_DIRECTORY} && \
+java org.antlr.v4.gui.TestRig ${PACKAGE}.${NAME} ${START} -gui ${FILE}
