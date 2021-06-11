4eb20c23e280e8fab1bbda6ac004ba1aae650462
昌维 <867597730@qq.com>
2021-06-11 17:12:15 +0800

    update test.sh: update source code path and build target path

--- test.sh
+++ test.sh
@@ -4,12 +4,18 @@
 # repo: https://github.com/cw1997/verilog-parser
 # data: 2021-06-11 02:55:25
 
+PACKAGE='me.changwei.verilog.parser'
 NAME='Verilog'
 START='start'
-FILE='../add2.v'
-OUTPUT_DIRECTORY='./output'
+FILE='../../../../../../../../add2.v'
+OUTPUT_DIRECTORY='./verilog'
+SUB_DIRECTORY='/me/changwei/verilog/parser'
+OUTPUT_SRC_DIRECTORY="${OUTPUT_DIRECTORY}/src/main/java${SUB_DIRECTORY}"
+OUTPUT_BIN_DIRECTORY="${OUTPUT_DIRECTORY}/target/classes"
+EXECUTABLE_BIN_DIRECTORY="${OUTPUT_BIN_DIRECTORY}${SUB_DIRECTORY}"
 
-java org.antlr.v4.Tool ./src/${NAME}.g4 -o ${OUTPUT_DIRECTORY} && \
-javac ${OUTPUT_DIRECTORY}/*.java && \
-cd ${OUTPUT_DIRECTORY} && \
+java org.antlr.v4.Tool -listener -visitor -long-messages ./src/${NAME}.g4 -o ${OUTPUT_SRC_DIRECTORY} -package ${PACKAGE} && \
+mkdir -p ${OUTPUT_BIN_DIRECTORY} && \
+javac ${OUTPUT_SRC_DIRECTORY}/*.java -d ${OUTPUT_BIN_DIRECTORY} && \
+cd ${EXECUTABLE_BIN_DIRECTORY} && \
 java org.antlr.v4.gui.TestRig ${NAME} ${START} -gui ${FILE}
